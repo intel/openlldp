22e9d0700784f8b8471bc88bf34f7ac315c11970
Thomas Richter <tmricht@linux.vnet.ibm.com>
2013-03-20 09:00:42 -0700

    vdp remove file lldp_vdp_utils.c
    Remove file lldp_vdp_utils.c and move its functions to
    file lldp_vdp.c.
    
    Signed-off-by: Thomas Richter <tmricht@linux.vnet.ibm.com>
    Signed-off-by: John Fastabend <john.r.fastabend@intel.com>

--- lldp_vdp.c
+++ lldp_vdp.c
@@ -98,6 +98,82 @@ void vdp_trace_profile(struct vsi_profile *p)
 	}
 }
 
+struct vsi_profile *vdp_alloc_profile()
+{
+	struct vsi_profile *prof;
+
+	prof = calloc(1, sizeof *prof);
+	if (prof)
+		LIST_INIT(&prof->macvid_head);
+	return prof;
+}
+
+/*
+ * vdp_remove_macvlan - remove all mac/vlan pairs in the profile
+ * @profile: profile to remove
+ *
+ * Remove all allocated <mac,vlan> pairs on the profile.
+ */
+static void vdp_remove_macvlan(struct vsi_profile *profile)
+{
+	struct mac_vlan *p;
+
+	while ((p = LIST_FIRST(&profile->macvid_head))) {
+		LIST_REMOVE(p, entry);
+		free(p);
+	}
+}
+
+void vdp_delete_profile(struct vsi_profile *prof)
+{
+	vdp_remove_macvlan(prof);
+	free(prof);
+}
+
+/* vdp_profile_equal - checks for equality of 2 profiles
+ * @p1: profile 1
+ * @p2: profile 2
+ *
+ * returns true if equal, false if not
+ *
+ * compares mgrid, id, version, instance 2 vsi profiles to find
+ * out if they are equal.
+ */
+static bool vdp_profile_equal(struct vsi_profile *p1, struct vsi_profile *p2)
+{
+	if (p1->mgrid != p2->mgrid)
+		return false;
+
+	if (p1->id != p2->id)
+		return false;
+
+	if (p1->version != p2->version)
+		return false;
+
+	if (memcmp(p1->instance, p2->instance, 16))
+		return false;
+
+	return true;
+}
+
+/*
+ * vdp_find_profile - Find a profile in the list of profiles already allocated
+ *
+ * Returns pointer to already allocated profile in list, 0 if not.
+ */
+
+struct vsi_profile *vdp_find_profile(struct vdp_data *vd,
+				     struct vsi_profile *thisone)
+{
+	struct vsi_profile *p;
+
+	LIST_FOREACH(p, &vd->profile_head, profile) {
+		if (vdp_profile_equal(p, thisone))
+			return p;
+	}
+	return 0;
+}
+
 /* vdp_data - searches vdp_data in the list of modules for this port
  * @ifname: interface name to search for
  *
