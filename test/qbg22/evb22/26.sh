3bc8b0d90453a3df0eb362ed96367e9449cd25ce
Thomas Richter <tmricht@linux.vnet.ibm.com>
2014-02-24 20:43:49 +0000

    evb22 test case 26
    Add a test case which disables EVB TLV transmission
    during execution.
    
    Signed-off-by: Thomas Richter <tmricht@linux.vnet.ibm.com>
    Signed-off-by: John Fastabend <john.r.fastabend@intel.com>

---
+++ test/qbg22/evb22/26.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#
+# Test case for LLDPAD EVB Testing according to IEEE 802.1Qbg
+#
+# Copyright (c) International Business Machines Corp., 2014
+#
+# Author(s): Thomas Richter <tmricht at linux.vnet.ibm.com>
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms and conditions of the GNU General Public License,
+# version 2, as published by the Free Software Foundation.
+#
+# This program is distributed in the hope it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+# more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin St - Fifth Floor, Boston, MA 02110-1301 USA.
+#
+# Configuration file for unknwon vsi data test cases
+#
+
+#
+# Thomas Richter, IBM Research & Development, Boeblingen, Germany, 16-Aug-2012
+#
+# Execute a single task on the machine running lldpad
+#
+
+# Wait 50 seconds change setting
+sleep 50;
+$1/lldptool -i veth0 -gncb -T -V evb -c enableTx=no
