intel/openlldp

commit 527ac435e46c59f9ee83aced36ccf83a0518f7fe

lldpad: Do not enable CEE mode if explicitly disabled
Do not re-enable CEE mode by default after it has been
explicitly disabled.

Signed-off-by: John Fastabend <john.r.fastabend@intel.com>

lldp_dcbx_cmds.c
@@ -245,7 +245,6 @@ static int get_dcb_state(char *port_id, char *rbuf)
 static dcb_result set_dcb_state(char *port_id, char *ibuf, int ilen)
 {
 	bool state;
-	int tmp;
 	int off;
 	int plen;
 	dcb_result rval = dcb_success;
@@ -255,14 +254,8 @@ static dcb_result set_dcb_state(char *port_id, char *ibuf, int ilen)
 
 	if (ilen == (off + CFG_DCB_DLEN)) {
 		state = (*(ibuf+off+DCB_STATE)) ^ '0';
-		/* if get_hw_state fails, then don't bother
-		 * trying to set the state.
-		 */
-		if (get_hw_state(port_id, &tmp) ||
-			set_hw_state(port_id, state))
-			rval = dcb_failed;
-		else
-			rval = save_dcb_enable_state(port_id, state);
+		set_hw_state(port_id, state);
+		rval = save_dcb_enable_state(port_id, state);
 	} else {
 		printf("error - setcommand has invalid argument length\n");
 		rval = dcb_bad_params;

lldp_dcbx.c
@@ -563,10 +563,15 @@ initialized:
 
 	/* if the dcbx field is not filled in by the capabilities
 	 * query, then the kernel is older and does not support
-	 * IEEE mode, so make CEE DCBX active by default.
+	 * IEEE mode, so make CEE DCBX active by default. Unless
+	 * the dcb state has been disabled from command line.
 	 */
 	get_dcb_capabilities(ifname, &dcb_support);
-	if (!dcb_support.dcbx || (gdcbx_subtype & ~MASK_DCBX_FORCE)) {
+
+	exists = get_dcb_enable_state(ifname, &dcb_enable);
+
+	if ((exists < 0 || dcb_enable) &&
+	    (!dcb_support.dcbx || (gdcbx_subtype & ~MASK_DCBX_FORCE))) {
 		set_dcbx_mode(tlvs->ifname,
 			      DCB_CAP_DCBX_HOST | DCB_CAP_DCBX_VER_CEE);
 		set_hw_state(ifname, 1);
@@ -719,8 +724,12 @@ int dcbx_rchange(struct port *port,  struct unpacked_tlv *tlv)
 	}
 
 	if (tlv->type == TYPE_0) {
+		int enabled;
+		int exists = get_dcb_enable_state(dcbx->ifname, &enabled);
+
 		if (!dcbx->active && !ieee8021qaz_tlvs_rxed(dcbx->ifname) &&
-		    dcbx->rxed_tlvs) {
+		    dcbx->rxed_tlvs &&
+		    (exists < 0 || enabled)) {
 			LLDPAD_INFO("CEE DCBX %s going ACTIVE\n", dcbx->ifname);
 			set_dcbx_mode(port->ifname,
 				      DCB_CAP_DCBX_HOST | DCB_CAP_DCBX_VER_CEE);

commit 9e9e9971b789a8f87e04a14916c974646149406b

DCB set state operation return value check
Added a return value check for 'set_dcb_state'. The function
didn't check whether the 'set_hw_state' was successful.
Without this check 'dcb sc <if> dcb [on/off]' returns 'Successful'
even when operation is not supported in driver.

Fixes: 527ac435e46c ("lldpad: Do not enable CEE mode if explicitly disabled")
Signed-off-by: Shay Agroskin <shayag@mellanox.com>

lldp_dcbx_cmds.c
@@ -259,8 +259,12 @@ static cmd_status set_dcb_state(char *port_id, char *ibuf, int ilen)
 
 	if (ilen == (off + CFG_DCB_DLEN)) {
 		state = (*(ibuf+off+DCB_STATE)) ^ '0';
-		set_hw_state(port_id, state);
-		rval = save_dcb_enable_state(port_id, state);
+
+		if(set_hw_state(port_id, state) < 0) {
+		    rval = cmd_not_capable;
+		} else {
+		    rval = save_dcb_enable_state(port_id, state);
+		}
 	} else {
 		printf("error - setcommand has invalid argument length\n");
 		rval = cmd_bad_params;