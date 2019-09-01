283e4902dfbef9ec64a386437bc87332d8674879
Loren M. Lang <lorenl@north-winds.org>
2019-08-31 19:28:30 -0700

    Place the tarball in the right place

--- contrib/build-rpm.sh
+++ contrib/build-rpm.sh
@@ -7,5 +7,5 @@ version="1.0.1"
 
 mkdir -p "$sources"
 #git archive --prefix=lldpad-"$version"/ --format=tar.gz --output="$sources"/lldpad-"$version".tar.gz HEAD
-tar --transform='s:^\.:lldpad-'"$version"':' --exclude='.git*' --exclude='.travis.yml' -cvzf ../lldpad-"$version".tar.gz .
+tar --transform='s:^\.:lldpad-'"$version"':' --exclude='.git*' --exclude='.travis.yml' -cvzf "$sources"/lldpad-"$version".tar.gz .
 #rpmbuild -ba lldpad.spec
