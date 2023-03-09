intel/openlldp

commit 2e442f9969582e3ae5dd53dda833f1cc961eff4b

dcbx: Free manifest in rchange callback
manifest is allocated in the ifup callback. Free it before reallocating
it in the rchange callback. This same leak may also occur if processing
of an LLPDU is aborted half way through.

Signed-off-by: Ross Lagerwall <ross.lagerwall@citrix.com>
Signed-off-by: Aaron Conole <aconole@redhat.com>

lldp_dcbx.c
@@ -695,6 +695,7 @@ int dcbx_rchange(struct port *port, struct lldp_agent *agent, struct unpacked_tl
 	 * present
 	 */
 	if (tlv->type == TYPE_1) {
+		clear_dcbx_manifest(dcbx);
 		manifest = malloc(sizeof(*manifest));
 		if (!manifest) {
 			LLDPAD_INFO("failed malloc for manifest\n");

commit 5eaba2a93cd31b29522494702058b065f2936fbd

dcbx: Fix NULL pointer dereference
A TLV with type == 0 followed a TLV with type == 1 results in
clear_dcbx_manifest() being called twice without a manifest allocation
in between. This results in a NULL pointer dereference accessing
manifest. Fix by checking for this in clear_dcbx_manifest().

At the same time, unify clear_dcbx_manifest() and dcbx_free_manifest()
since they are basically the same and make clear_dcbx_manifest() static.

Fixes: 2e442f996958 ("dcbx: Free manifest in rchange callback")
Signed-off-by: Ross Lagerwall <ross.lagerwall@citrix.com>

lldp_dcbx.c
@@ -57,6 +57,7 @@ extern u8 gdcbx_subtype;
 
 void dcbx_free_tlv(struct dcbx_tlvs *tlvs);
 static int dcbx_check_operstate(struct port *port, struct lldp_agent *agent);
+static void clear_dcbx_manifest(struct dcbx_tlvs *dcbx);
 
 const struct lldp_mod_ops dcbx_ops = {
 	.lldp_mod_register	= dcbx_register,
@@ -279,29 +280,6 @@ fail_add:
 	return -1;
 }
 
-void dcbx_free_manifest(struct dcbx_manifest *manifest)
-{
-	if (!manifest)
-		return;
-
-	if (manifest->dcbx1)
-		free_unpkd_tlv(manifest->dcbx1);
-	if (manifest->dcbx2)
-		free_unpkd_tlv(manifest->dcbx2);
-	if (manifest->dcbx_ctrl)
-		free_unpkd_tlv(manifest->dcbx_ctrl);
-	if (manifest->dcbx_pg)
-		free_unpkd_tlv(manifest->dcbx_pg);
-	if (manifest->dcbx_pfc)
-		free_unpkd_tlv(manifest->dcbx_pfc);
-	if (manifest->dcbx_app)
-		free_unpkd_tlv(manifest->dcbx_app);
-	if (manifest->dcbx_llink)
-		free_unpkd_tlv(manifest->dcbx_llink);
-	free(manifest);
-	return;
-}
-
 void dcbx_free_tlv(struct dcbx_tlvs *tlvs)
 {
 	if (!tlvs)
@@ -371,7 +349,7 @@ static void dcbx_free_data(struct dcbd_user_data *dud)
 			dd = LIST_FIRST(&dud->head);
 			LIST_REMOVE(dd, entry);
 			dcbx_free_tlv(dd);
-			dcbx_free_manifest(dd->manifest);
+			clear_dcbx_manifest(dd);
 			free(dd);
 		}
 	}
@@ -639,13 +617,13 @@ void dcbx_ifdown(char *device_name, struct lldp_agent *agent)
 
 	LIST_REMOVE(tlvs, entry);
 	dcbx_free_tlv(tlvs);
-	dcbx_free_manifest(tlvs->manifest);
+	clear_dcbx_manifest(tlvs);
 	free(tlvs);
 }
 
-void clear_dcbx_manifest(struct dcbx_tlvs *dcbx)
+static void clear_dcbx_manifest(struct dcbx_tlvs *dcbx)
 {
-	if (!dcbx)
+	if (!dcbx || !dcbx->manifest)
 		return;
 
 	if (dcbx->manifest->dcbx_llink)