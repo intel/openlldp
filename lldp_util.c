intel/openlldp

commit 60bc0adf198b12cfffafef4176aacebd745a0a04

lldpad: handle more than 8 lldptool parameters
lldpad was only properly handling up to 8 lldptool
TLV parameters.  However, it was not checking for
more than 8, so bad things, up to lldpad segfault
could occur.  This patch allows handling any number
of TLV parameters for get or set commands.

Signed-off-by: Eric Multanen <eric.w.multanen@intel.com>
Signed-off-by: John Fastabend <john.r.fastabend@intel.com>

lldp_util.c
@@ -1229,24 +1229,45 @@ int check_link_status(const char *ifname)
 	return retval;
 }
 
+#define NUM_ARGS 8
+
 int get_arg_val_list(char *ibuf, int ilen, int *ioff,
 			    char **args, char **argvals)
 {
 	u8 arglen;
 	u16 argvalue_len;
-	int arglens[8];
-	int argvallens[8];
+	int *arglens = NULL;
+	int *argvallens = NULL;
+	int *p;
 	int numargs;
 	int i;
 
 	/* parse out args and argvals */
 	for (i = 0; ilen - *ioff > 2 * (int)sizeof(arglen); i++) {
+		if (!(i % NUM_ARGS)) {
+			p = (int *) realloc(arglens,
+				(i/NUM_ARGS + 1) * NUM_ARGS * sizeof(int));
+			if (!p) {
+				free(arglens);
+				return 0;
+			} else {
+				arglens = p;
+			}
+			p = (int *) realloc(argvallens,
+				(i/NUM_ARGS + 1) * NUM_ARGS * sizeof(int));
+			if (!p) {
+				free(argvallens);
+				return 0;
+			} else {
+				argvallens = p;
+			}
+		}
 		hexstr2bin(ibuf+*ioff, &arglen, sizeof(arglen));
 		*ioff += 2 * (int)sizeof(arglen);
 		if (ilen - *ioff >= arglen) {
 			args[i] = ibuf+*ioff;
 			*ioff += arglen;
-			arglens[i] = arglen;
+			*(arglens+i) = arglen;
 
 			if (ilen - *ioff > 2 * (int)sizeof(argvalue_len)) {
 				hexstr2bin(ibuf+*ioff, (u8 *)&argvalue_len,
@@ -1256,46 +1277,65 @@ int get_arg_val_list(char *ibuf, int ilen, int *ioff,
 				if (ilen - *ioff >= argvalue_len) {
 					argvals[i] = ibuf+*ioff;
 					*ioff += argvalue_len;
-					argvallens[i] = argvalue_len;
+					*(argvallens+i) = argvalue_len;
 				}
 			} else {
+				free(arglens);
+				free(argvallens);
 				return 0;
 			}
 		} else {
+			free(arglens);
+			free(argvallens);
 			return 0;
 		}
 	}
 	numargs = i;
 	for (i = 0; i < numargs; i++) {
-		args[i][arglens[i]] = '\0';
-		argvals[i][argvallens[i]] = '\0';
+		args[i][*(arglens+i)] = '\0';
+		argvals[i][*(argvallens+i)] = '\0';
 	}
+	free(arglens);
+	free(argvallens);
 	return numargs;
 }
 
 int get_arg_list(char *ibuf, int ilen, int *ioff, char **args)
 {
 	u8 arglen;
-	int arglens[8];
+	int *arglens = NULL;
+	int *p;
 	int numargs;
 	int i;
 
 	/* parse out args */
 	for (i = 0; (ilen - *ioff > 2 * (int)sizeof(arglen)); i++) {
+		if (!(i % NUM_ARGS)) {
+			p = (int *) realloc(arglens,
+				(i/NUM_ARGS + 1) * NUM_ARGS * sizeof(int));
+			if (!p) {
+				free(arglens);
+				return 0;
+			} else {
+				arglens = p;
+			}
+		}
 		hexstr2bin(ibuf+(*ioff), &arglen, sizeof(arglen));
 		*ioff += 2*sizeof(arglen);
 		if (ilen - *ioff >= arglen) {
 			args[i] = ibuf+(*ioff);
 			*ioff += arglen;
-			arglens[i] = arglen;
+			*(arglens+i) = arglen;
 		} else {
+			free(arglens);
 			return 0;
 		}
 	}
 	numargs = i;
 
 	for (i = 0; i < numargs; i++)
-		args[i][arglens[i]] = '\0';
+		args[i][*(arglens+i)] = '\0';
 
+	free(arglens);
 	return numargs;
 }

commit 203c18b2cf1a7e6e1b0e934c9c59b7bcf57c96d1

lldp_util: fix resource leak
The arglens and argvallens variables are realloced in the loop. So when
there is a failure, both of them should be freed. Let's make numargs = 0
and goto err_out directly.

Fixes: 60bc0adf198b ("lldpad: handle more than 8 lldptool parameters")
Signed-off-by: Hangbin Liu <liuhangbin@gmail.com>

lldp_util.c
@@ -1279,16 +1279,16 @@ int get_arg_val_list(char *ibuf, int ilen, int *ioff,
 			p = (int *) realloc(arglens,
 				(i/NUM_ARGS + 1) * NUM_ARGS * sizeof(int));
 			if (!p) {
-				free(arglens);
-				return 0;
+				numargs = 0;
+				goto err_out;
 			} else {
 				arglens = p;
 			}
 			p = (int *) realloc(argvallens,
 				(i/NUM_ARGS + 1) * NUM_ARGS * sizeof(int));
 			if (!p) {
-				free(argvallens);
-				return 0;
+				numargs = 0;
+				goto err_out;
 			} else {
 				argvallens = p;
 			}
@@ -1311,14 +1311,12 @@ int get_arg_val_list(char *ibuf, int ilen, int *ioff,
 					*(argvallens+i) = argvalue_len;
 				}
 			} else {
-				free(arglens);
-				free(argvallens);
-				return 0;
+				numargs = 0;
+				goto err_out;
 			}
 		} else {
-			free(arglens);
-			free(argvallens);
-			return 0;
+			numargs = 0;
+			goto err_out;
 		}
 	}
 	numargs = i;
@@ -1326,6 +1324,8 @@ int get_arg_val_list(char *ibuf, int ilen, int *ioff,
 		args[i][*(arglens+i)] = '\0';
 		argvals[i][*(argvallens+i)] = '\0';
 	}
+
+err_out:
 	free(arglens);
 	free(argvallens);
 	return numargs;