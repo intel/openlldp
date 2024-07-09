intel/openlldp

commit b4f9ebc96e0cc29ec2eeca69de94afaf64e0dd8c

lldpad: implement APP TLV set/get commands
Extend Application TLV set/get commands to process multiple
applications.

Signed-off-by: John Fastabend <john.r.fastabend@intel.com>

lldp_8021qaz.c
@@ -345,6 +345,40 @@ static int read_cfg_file(char *ifname, struct ieee8021qaz_tlvs *tlvs,
 	res = get_config_setting(ifname, arg_path, &numtcs, CONFIG_TYPE_INT);
 	tlvs->pfc->local.pfc_cap = numtcs;
 
+	/* Read and add APP data to internal lldpad APP ring */
+	for (i = 0; ; i++) {
+		char *parse;
+		char *app_tuple;
+		u8 prio, sel;
+		u16 pid;
+
+		snprintf(arg_path, sizeof(arg_path), "%s%08x.%s%i", TLVID_PREFIX,
+			 TLVID_8021(LLDP_8021QAZ_APP), ARG_APP, i);
+		res = get_config_setting(ifname, arg_path, &arg, CONFIG_TYPE_STRING);
+
+		if (res)
+			break;
+
+		parse = strdup(arg);
+		if (!parse)
+			break;
+		app_tuple = strtok(parse, ",");
+		if (!app_tuple)
+			break;
+		prio = atoi(app_tuple);
+		app_tuple = strtok(NULL, ",");
+		if (!app_tuple)
+			break;
+		sel = atoi(app_tuple);
+
+		app_tuple = strtok(NULL, ",");
+		if (!app_tuple)
+			break;
+		pid = atoi(app_tuple);
+		free(parse);
+		ieee8021qaz_add_app(&tlvs->app_head, 0, prio, sel, pid);
+	}
+
 	return 0;
 }
 
@@ -609,7 +643,7 @@ void print_pfc(struct ieee_pfc *pfc)
 }
 #endif
 
-struct app_prio *get_ieee_app(const char *ifname, int *cnt)
+static struct app_prio *get_ieee_app(const char *ifname, int *cnt)
 {
 	int err = 0;
 	int rem;

lldp_8021qaz_cmds.c
@@ -868,9 +868,9 @@ static int get_arg_app(struct cmd *cmd, char *args, char *arg_value,
 		       char *obuf, int obuf_len)
 {
 	struct ieee8021qaz_tlvs *tlvs;
-	struct app_prio *apps;
-	int cnt, i;
-	char temp[80], app_buf[1024] = "(prio,sel,proto)\n";
+	int  i = 0;
+	struct app_obj *np;
+	char app_buf[1024] = "(prio,sel,proto)\n";
 
 	if (cmd->cmd != cmd_gettlv)
 		return cmd_invalid;
@@ -888,22 +888,30 @@ static int get_arg_app(struct cmd *cmd, char *args, char *arg_value,
 		return cmd_not_applicable;
 	}
 
-	apps = get_ieee_app(cmd->ifname, &cnt);
-	for (i = 0; apps && i < cnt; i++) {
-		if ((apps[i].prs & 0x07) == 1) {
-			snprintf(temp, sizeof(temp), "%i:(%i,%i,0x%0004x)\n", i,
-				(apps[i].prs & 0xE0) >> 5,
-				(apps[i].prs & 0x07),
-				apps[i].pid);
+	LIST_FOREACH(np, &tlvs->app_head, entry) {
+		char new_app[80];
+		struct dcb_app *dcb_app = &np->app;
+
+		if (dcb_app->selector == 1) {
+			snprintf(new_app, sizeof(new_app),
+				"%i:(%i,%i,0x%04x) %s hw %i\n", i,
+				dcb_app->priority,
+				dcb_app->selector,
+				dcb_app->protocol,
+				np->peer ? "peer" : "local",
+				np->hw);
 		} else {
-			snprintf(temp, sizeof(temp), "%i:(%i,%i,%i)\n", i,
-				(apps[i].prs & 0xE0) >> 5,
-				(apps[i].prs & 0x07),
-				ntohs(apps[i].pid));
+			snprintf(new_app, sizeof(new_app),
+				"%i:(%i,%i,%i) %s hw %i\n", i,
+				dcb_app->priority,
+				dcb_app->selector,
+				dcb_app->protocol,
+				np->peer ? "peer" : "local",
+				np->hw);
 		}
-		strncat(app_buf, temp, sizeof(app_buf) - strlen(app_buf) - 1);
+		strncat(app_buf, new_app, sizeof(app_buf) - strlen(obuf) - 2);
+		i++;
 	}
-	free(apps);
 
 	sprintf(obuf, "%02x%s%04x%s",
 		(unsigned int) strlen(args), args,
@@ -921,9 +929,8 @@ static int _set_arg_app(struct cmd *cmd, char *args, char *arg_value,
 	char arg_path[256];
 	u8 prio, sel;
 	u16 pid;
-	struct app_prio *apps;
-	int cnt, i;
-	char temp[80], app_buf[1024] = "(prio,sel,proto)\n";
+	struct app_obj *np;
+	int i, res;
 
 	if (cmd->cmd != cmd_settlv)
 		return cmd_invalid;
@@ -967,26 +974,51 @@ static int _set_arg_app(struct cmd *cmd, char *args, char *arg_value,
 	ieee8021qaz_add_app(&tlvs->app_head, 0, prio, sel, pid);
 	ieee8021qaz_app_sethw(cmd->ifname, &tlvs->app_head);
 
-	apps = get_ieee_app(cmd->ifname, &cnt);
-	for (i = 0; i < cnt; i++) {
-		if ((apps[i].prs & 0x07) == 1) {
-			snprintf(obuf, obuf_len,
-				"%i:(%i,%i,0x%0004x) ", i,
-				(apps[i].prs & 0xE0) >> 5,
-				(apps[i].prs & 0x07),
-				apps[i].pid);
+	i = 0;
+	LIST_FOREACH(np, &tlvs->app_head, entry) {
+		char new_app[80];
+		struct dcb_app *dcb_app = &np->app;
+
+		if (dcb_app->selector == 1) {
+			snprintf(new_app, sizeof(new_app),
+				"%i:(%i,%i,0x%04x) %s hw %i\n", i,
+				dcb_app->priority,
+				dcb_app->selector,
+				dcb_app->protocol,
+				np->peer ? "peer" : "local",
+				np->hw);
 		} else {
-			snprintf(obuf, obuf_len,
-				"%i:(%i,%i,%i) ", i,
-				(apps[i].prs & 0xE0) >> 5,
-				(apps[i].prs & 0x07),
-				ntohs(apps[i].pid));
+			snprintf(new_app, sizeof(new_app),
+				"%i:(%i,%i,%i) %s hw %i\n", i,
+				dcb_app->priority,
+				dcb_app->selector,
+				dcb_app->protocol,
+				np->peer ? "peer" : "local",
+				np->hw);
 		}
+		strncat(obuf, new_app, obuf_len - strlen(obuf) - 2);
+		i++;
 	}
-	strncat(obuf, "\n", obuf_len - strlen(obuf) - 1);
+
+	/* Count APP entries in config file */
+	for (i = 0; ; i++) {
+		char *dummy;
+
+		snprintf(arg_path, sizeof(arg_path), "%s%08x.%s%i", TLVID_PREFIX,
+			 TLVID_8021(LLDP_8021QAZ_APP), ARG_APP, i);
+		res = get_config_setting(cmd->ifname, arg_path, &dummy,
+					 CONFIG_TYPE_STRING);
+
+		if (res)
+			break;
+
+		if (strcmp(dummy, arg_value) == 0)
+			return cmd_success;
+	}
 
 	snprintf(arg_path, sizeof(arg_path),
-		 "%s%08x.%s", TLVID_PREFIX, cmd->tlvid, args);
+		 "%s%08x.%s%i", TLVID_PREFIX, cmd->tlvid, args, i);
+
 	set_config_setting(cmd->ifname, arg_path, &arg_value,
 			   CONFIG_TYPE_STRING);
 

include/lldp_8021qaz.h
@@ -204,8 +204,6 @@ struct ieee8021qaz_user_data {
 	LIST_HEAD(ieee8021qaz_head, ieee8021qaz_tlvs) head;
 };
 
-struct app_prio *get_ieee_app(const char *ifname, int *cnt);
-
 int ieee8021qaz_add_app(struct app_tlv_head *head, int peer,
 			u8 prio, u8 sel, u16 proto);
 int ieee8021qaz_app_sethw(char *ifname, struct app_tlv_head *head);

commit 9bf267efcba961d796980fc4f18ffa609045213d

lldpad: free the memory allocted by strdup
There are some strdup calls that are not freed after use. Let's free them
to avoid resource leaks.

This issue was found through static code analysis.

Fixes: b4f9ebc96e0c ("lldpad: implement APP TLV set/get commands")
Fixes: 0313734d05cc ("lldpad: correct IEEE DCBX capabilities check")
Signed-off-by: Hangbin Liu <liuhangbin@gmail.com>

lldp_8021qaz.c
@@ -210,6 +210,7 @@ static int read_cfg_file(char *ifname, struct lldp_agent *agent,
 	char arg_path[256];
 	int res = 0, i;
 	int willing, pfc_mask, delay;
+	char *parse = NULL;
 
 	if (agent->type != NEAREST_BRIDGE)
 		return 0;
@@ -354,7 +355,6 @@ static int read_cfg_file(char *ifname, struct lldp_agent *agent,
 
 	/* Read and add APP data to internal lldpad APP ring */
 	for (i = 0; i < MAX_APP_ENTRIES; i++) {
-		char *parse;
 		char *app_tuple;
 		u8 prio, sel;
 		long pid;
@@ -391,8 +391,10 @@ static int read_cfg_file(char *ifname, struct lldp_agent *agent,
 			ieee8021qaz_mod_app(&tlvs->app_head, 0,
 					    prio, sel, (u16) pid, 0);
 		free(parse);
+		parse = NULL;
 	}
 
+	free(parse);
 	return 0;
 }
 

lldp_8021qaz_cmds.c
@@ -538,9 +538,9 @@ _set_arg_up2tc(struct cmd *cmd, char *args, const char *arg_value,
 	}
 
 	if (test) {
+		free(parse);
 		if (!is_dcbx_hw(cmd->ifname))
 			return cmd_not_capable;
-		free(parse);
 		return cmd_success;
 	}
 
@@ -648,16 +648,15 @@ _set_arg_tcbw(struct cmd *cmd, char *args, const char *arg_value,
 		percent[i] = atoi(toked_bw);
 		toked_bw = strtok(NULL, ",");
 	}
+	free(parse);
 
 	for (i = 0; i < 8; i++)
 		total += percent[i];
 	if (total != 100) {
-		err = cmd_invalid;
-		goto invalid;
+		return cmd_invalid;
 	} else if (test) {
 		if (!is_dcbx_hw(cmd->ifname))
 			return cmd_not_capable;
-		free(parse);
 		return cmd_success;
 	} else if (tcbw) {
 		memcpy(tcbw, percent, sizeof(*tcbw) * MAX_TCS);
@@ -677,8 +676,7 @@ _set_arg_tcbw(struct cmd *cmd, char *args, const char *arg_value,
 	set_config_setting(cmd->ifname, cmd->type, arg_path, &arg_value,
 			   CONFIG_TYPE_STRING);
 	somethingChangedLocal(cmd->ifname, cmd->type);
-invalid:
-	free(parse);
+
 	return err;
 }
 
@@ -805,9 +803,9 @@ _set_arg_tsa(struct cmd *cmd, char *args, const char *arg_value,
 	}
 
 	if (test) {
+		free(parse);
 		if (!is_dcbx_hw(cmd->ifname))
 			return cmd_not_capable;
-		free(parse);
 		return cmd_success;
 	}
 
@@ -935,11 +933,15 @@ static int _set_arg_enabled(struct cmd *cmd, char *args,
 			errno = 0;
 			prio = strtol(priority, &end, 10);
 
-			if (end == priority || *end != '\0')
-				return cmd_invalid;
+			if (end == priority || *end != '\0') {
+				err = cmd_invalid;
+				goto invalid;
+			}
 
-			if (errno || prio < 0)
-				return cmd_invalid;
+			if (errno || prio < 0) {
+				err = cmd_invalid;
+				goto invalid;
+			}
 
 			if (prio > 7) {
 				err = cmd_invalid;
@@ -951,9 +953,9 @@ static int _set_arg_enabled(struct cmd *cmd, char *args,
 	}
 
 	if (test) {
+		free(parse);
 		if (!is_dcbx_hw(cmd->ifname))
 			return cmd_not_capable;
-		free(parse);
 		return cmd_success;
 	}
 
@@ -1121,6 +1123,7 @@ static int get_arg_app(struct cmd *cmd, char *args, UNUSED char *arg_value,
 	const char *app;
 	u8 prio, sel;
 	int proto, hw = -1, i;
+	char *parse = NULL;
 
 	if (cmd->cmd != cmd_gettlv)
 		return cmd_invalid;
@@ -1137,7 +1140,7 @@ static int get_arg_app(struct cmd *cmd, char *args, UNUSED char *arg_value,
 	tlvs = ieee8021qaz_data(cmd->ifname);
 	for (i = 0; i < MAX_APP_ENTRIES; i++) {
 		char arg_path[256];
-		char *parse, *app_tuple;
+		char *app_tuple;
 		int err;
 
 		snprintf(arg_path, sizeof(arg_path), "%s%08x.%s%i",
@@ -1191,7 +1194,12 @@ static int get_arg_app(struct cmd *cmd, char *args, UNUSED char *arg_value,
 
 		arg_app_strncat_hw(new_app, hw);
 		strncat(app_buf, new_app, sizeof(app_buf) - strlen(app_buf) - 2);
+
+		free(parse);
+		parse = NULL;
 	}
+
+	free(parse);
 
 	if (tlvs) {
 		LIST_FOREACH(np, &tlvs->app_head, entry) {