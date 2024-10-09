intel/openlldp

commit a75e35a0cb36817e42a69ac14d1785cc63341132

lldpad: remove device from config file when ports are removed
When ports are removed, there is no need to keep them in the configuration
file. Otherwise, the port list could grow too large to manage. For example,
if a user adds hundreds of virtual ports for testing and then removes them
after the test, the configuration file would retain this information
indefinitely. Removing these entries ensures the configuration file remains
manageable.

Signed-off-by: Hangbin Liu <liuhangbin@gmail.com>
Signed-off-by: Aaron Conole <aconole@redhat.com>

config.c
@@ -472,6 +472,32 @@ int get_config_setting(const char *ifname, int agenttype, char *path,
 	return (rval == CONFIG_FALSE) ? cmd_failed : cmd_success;
 }
 
+int remove_config_device(const char *ifname, int agenttype)
+{
+	const char *section = agent_type2section(agenttype);
+	config_setting_t *setting = NULL;
+	int rval = cmd_failed;
+
+	setting = config_lookup(&lldpad_cfg, section);
+
+	if (setting != NULL) {
+		rval = config_setting_remove(setting, ifname);
+		if (rval == CONFIG_TRUE) {
+			LLDPAD_DBG("In %s, remove %s from section %s\n", __func__, ifname, section);
+			if (config_write_file(&lldpad_cfg, cfg_file_name)) {
+				rval = cmd_success;
+			} else {
+				LLDPAD_DBG("In %s, update config failed\n", __func__);
+				rval = cmd_failed;
+			}
+		}
+	} else {
+		LLDPAD_DBG("In %s, can't find section %s\n", __func__, section);
+	}
+
+	return rval;
+}
+
 int remove_config_setting(const char *ifname, int agenttype, char *parent,
 			  char *name)
 {

lldp/ports.c
@@ -355,6 +355,9 @@ int remove_port(const char *ifname)
 			free(agent->tx.frameout);
 
 		LIST_REMOVE(agent, entry);
+
+		remove_config_device(ifname, agent->type);
+
 		free(agent);
 	}
 

include/config.h
@@ -79,6 +79,7 @@ int get_cfg(const char *ifname, int agenttype, char *path, union cfg_get value, 
 int set_cfg(const char *ifname, int agenttype, char *path, union cfg_set value, int type);
 int get_config_setting(const char *ifname, int agenttype, char *path, union cfg_get value, int type);
 int set_config_setting(const char *ifname, int agenttype, char *path, union cfg_set value, int type);
+int remove_config_device(const char *ifname, int agenttype);
 int remove_config_setting(const char *ifname, int agenttype, char *parent, char *name);
 int get_config_tlvfield(const char *ifname, int agenttype, u32 tlvid, const char *field, union cfg_get value, int type);
 int get_config_tlvfield_int(const char *ifname, int agenttype, u32 tlvid, const char *field, int *value);

commit 23b2a8bd28e36976c87f8c351a63d262eae3aa53

lldpad: do not remove config when stopping lldpad
When lldpad is terminated by systemd or kill, the remove_all_adapters()
function is called. However, this function is intended to release port
resources, and the configuration file should not be destroyed. We only
need to remove the port when it is explicitly deleted. Otherwise, all
configurations will be lost when lldpad is stopped.

Fixes: a75e35a0cb36 ("lldpad: remove device from config file when ports are removed")
Reported-by: Fei Liu <feliu@redhat.com>
Signed-off-by: Hangbin Liu <liuhangbin@gmail.com>

config.c
@@ -135,7 +135,7 @@ void scan_port(UNUSED void *eloop_data, UNUSED void *user_ctx)
 		}
 		next = port->next;
 		if (!found)
-			remove_port(port->ifname);
+			remove_port(port->ifname, true);
 	}
 
 	/* Walk port list looking for devices that should have been added

event_iface.c
@@ -297,7 +297,7 @@ static void event_if_decode_nlmsg(int route_type, void *data, int len)
 			if (route_type == RTM_DELLINK) {
 				LLDPAD_INFO("%s: %s: device removed!\n",
 					__func__, device_name);
-				remove_port(device_name);
+				remove_port(device_name, true);
 			}
 			break;
 		case IF_OPER_DORMANT:

lldp/ports.c
@@ -297,7 +297,7 @@ fail:
 	return NULL;
 }
 
-int remove_port(const char *ifname)
+int remove_port(const char *ifname, bool remove_config)
 {
 	int ifindex = get_ifidx(ifname);
 	struct port *port;    /* Pointer to port to remove */
@@ -356,7 +356,8 @@ int remove_port(const char *ifname)
 
 		LIST_REMOVE(agent, entry);
 
-		remove_config_device(ifname, agent->type);
+		if (remove_config)
+			remove_config_device(ifname, agent->type);
 
 		free(agent);
 	}

lldp/ports.h
@@ -97,7 +97,7 @@ extern struct port *porthead;
 extern "C" {
 #endif
 struct port *add_port(int ifindex, const char *);
-int remove_port(const char *);
+int remove_port(const char *, bool remove_config);
 #ifdef __cplusplus
 }
 #endif

lldpad.c
@@ -167,7 +167,7 @@ static void remove_all_adapters(void)
 
 	for (port = porthead; port; port = next) {
 		next = port->next;
-		remove_port(port->ifname);
+		remove_port(port->ifname, false);
 	}
 
 	return;